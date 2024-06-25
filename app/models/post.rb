a8d3bd329065aef0433782a171c2e3fcfe8b86eb
sanyakosivchuk <kosivcuks@gmail.com>
2024-06-25 10:51:25 +0300

    Update model to handle photos

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,7 @@ class Post < ApplicationRecord
   belongs_to :blog
   belongs_to :user
   has_many :comments, dependent: :destroy
-
+  has_one_attached :photo
+  
   validates :content, presence: true, length: { minimum: 15 }
 end
